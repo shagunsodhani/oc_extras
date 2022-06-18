4f862a4a5ee75dfdd3a34fc2bcc5a2d7c406da5c
Shagun Sodhani <sodhani@fb.com>
2022-06-18 09:16:02 -0700

    Update tests

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import os
 import nox
 from nox.sessions import Session
 
-PROJECT_NAME = "oc_extras"
+PROJECT_DIR = "src"
 
 DEFAULT_PYTHON_VERSIONS = ["3.7", "3.8", "3.9"]
 
@@ -15,18 +15,19 @@ PYTHON_VERSIONS = os.environ.get(
 
 def setup(session: Session) -> None:
     session.install("--upgrade", "setuptools", "pip")
+    session.install(".")
     session.install("-r", "requirements/dev.txt")
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def lint(session: Session) -> None:
     setup(session=session)
-    session.run("flake8", PROJECT_NAME)
-    session.run("black", "--check", PROJECT_NAME)
-    session.run("isort", PROJECT_NAME, "--check", "--diff")
+    session.run("flake8", PROJECT_DIR)
+    session.run("black", "--check", PROJECT_DIR)
+    session.run("isort", PROJECT_DIR, "--check", "--diff")
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def mypy(session: Session) -> None:
     setup(session=session)
-    session.run("mypy", "--strict", PROJECT_NAME)
+    session.run("mypy", "--strict", PROJECT_DIR)
